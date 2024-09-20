79cad60778b65901b5d33c9a41e8b83ced26634a
Gabriel Tassa <tassacesar@gmail.com>
2024-09-19 23:24:06 -0300

    :construction: refactor: estruturando app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ConfirmacaoComponent } from './components/confirmacao/confirmacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'cadastro', 
+    component: CadastroComponent 
+  },
+  { 
+    path: 'confirmacao', 
+    component: ConfirmacaoComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
