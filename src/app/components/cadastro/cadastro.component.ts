6ac01ed521a3d42a9f3eecab1b04945ae8fdab4a
Gabriel Tassa <tassacesar@gmail.com>
2024-09-20 00:04:23 -0300

    :dizzy: adicionando rota e um emoji à tela de confirmação de conta

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CadastroComponent {
 
+  constructor(
+    private router: Router,
+  ) {}
+
+  goTo() {
+    this.router.navigate(['/confirmacao']);
+  }
 }
